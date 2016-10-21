94a220e02e9ee73499bf20a8e25d7e03fe175b40
Stealthmate <stealthmate1233@gmail.com>
2016-10-21 18:34:36 +0300

    Implemented

--- jp/details.js
+++ jp/details.js
@@ -9,67 +9,6 @@ const WHITESPACE = /[ \n\t]+/g;
 
 const WORDCLASS = /(^|\n)\[[^\[\]]+\]/g;
 
-function parseDetails(html, resolve) {
-    let $ = require('cheerio').load(html);
-
-    let glosses = $("dl.lst > dt");
-    let examplelists = $("dl.lst > dd");
-
-    let glossesobjs = [];
-
-    for (let i = 0; i <= glosses.length - 1; i++) {
-        let glossobj = {};
-        glossobj.def = $(glosses[i]).find(".title").children("strong").text().replace(WHITESPACE, " ");
-        glossobj.ex = [];
-        let exlist = $(examplelists[i]).find(".lst_mean > li");
-        for (let j = 0; j <= exlist.length - 1; j++) {
-            glossobj.ex[j] = $(exlist[j]).children().remove("span").end().text().trim().replace(WHITESPACE, " ");
-        }
-
-        glossesobjs.push(glossobj);
-    }
-
-    if (glosses.length == 0) {
-        glosses = $("#meanArea > .pclass");
-
-        let glossobj = {
-            def: glosses.text().trim().replace(WHITESPACE, " "),
-            ex: []
-        };
-
-        let exlist = $("#meanArea > ul.lst_mean > li");
-        for (let j = 0; j <= exlist.length - 1; j++) {
-            glossobj.ex[j] = $(exlist[j]).text().trim().replace(WHITESPACE, " ");
-        }
-
-        glossesobjs.push(glossobj)
-    }
-
-    resolve(glossesobjs);
-
-}
-
-function lookUp(link) {
-    return new Promise((resolve, reject) => {
-
-        let http = require('http');
-
-        REQUEST_OPTIONS.path = "/" + link;
-
-        let req = http.request(REQUEST_OPTIONS, function(res) {
-            res.setEncoding('utf8');
-            var html = "";
-            res.on('data', function(chunk) {
-                    html = html + chunk;
-                })
-                .on('end', () => {
-                    parseDetails(html, resolve);
-                });
-        });
-        req.end();
-    })
-}
-
 const DETAILS_KANJI = "hanja";
 
 function serve(req, res) {
